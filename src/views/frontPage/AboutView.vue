8e6b9245c9d563c4016774a10b2a0f48c44270e3
caijiW <caiji202111@gmail.com>
2023-03-21 04:48:27 +0800

    modified

--- src/views/frontPage/AboutView.vue
+++ src/views/frontPage/AboutView.vue
@@ -3,7 +3,9 @@
     <h2>This is an about page</h2>
     <h3 class="test-wahaha">ㄏ哈哈哈哈哈哈</h3>
   </div>
-
+  <div>
+    <img src="/frontPage/amir-test.jpg" alt="" style="width:500px">
+  </div>
 </template>
 
 <style>
